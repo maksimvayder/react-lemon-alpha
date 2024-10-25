e76adf326fe65e68b427cef4d9f9b4eed0d60433
Maksim <teseygrecia@gmail.com>
2024-10-25 19:05:23 +0300

    Test_part1

---
+++ cypress/e2e/test_music_tack.cy.js
@@ -0,0 +1,25 @@
+/// <reference types='cypress' />
+import MusicAppPage from '../support/pages/MusicAppPage';
+
+describe('Music App - Core Features', () => {
+  beforeEach(() => {
+    MusicAppPage.visit();
+  });
+
+  it('should filter tracks based on the search input', () => {
+    const searchQuery = 'Winter Winds';
+    MusicAppPage.searchForTrack(searchQuery);
+    MusicAppPage.verifySearchResultsContain(searchQuery);
+  });
+
+  it('should add a single track to "Your Playlist" when clicking the "+" button', () => {
+    MusicAppPage.addFirstTrackToPlaylist();
+    MusicAppPage.verifyPlaylistLength(1);
+  });
+
+  it('should add multiple selected tracks to "Your Playlist"', () => {
+    MusicAppPage.checkMultipleTracks([1, 2, 3]);
+    MusicAppPage.addSelectedTracks();
+    MusicAppPage.verifyAddedTracksCount(3);
+  });
+});
